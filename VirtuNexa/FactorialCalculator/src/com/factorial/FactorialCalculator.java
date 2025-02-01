7dab1e72ee9be037258e50f58264c9b016eb9c8d
ronaksorout09dec <87173646+ronaksorout09dec@users.noreply.github.com>
2025-02-01 22:00:14 +0530

    Delete VirtuNexa/FactorialCalculator/src/com/factorial directory

--- VirtuNexa/FactorialCalculator/src/com/factorial/FactorialCalculator.java
+++
@@ -1,8 +0,0 @@
-package com.factorial;
-
-public class FactorialCalculator {
-    public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        ui.start();
-    }
-}
